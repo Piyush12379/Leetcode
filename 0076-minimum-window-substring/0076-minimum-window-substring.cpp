2be3e704483d35ed3569236abd086b8bc0bc2b03
Piyush12379 <150358735+Piyush12379@users.noreply.github.com>
2026-02-02 18:42:56 +0530

    Time: 91 ms (9.57%), Space: 11.6 MB (26.97%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
+
+    bool sahi(unordered_map<char,int>&have,unordered_map<char,int>&required){
+         
+         for(auto it: have){
+               if(it.second>required[it.first]){
+                return false;
+               }
+         }
+         return true;
+
+    }
+
+
     string minWindow(string s, string t) {
-        if(t.size() > s.size()) return "";
+        unordered_map<char,int>have;
+        unordered_map<char,int>required;
+        int minlen=INT_MAX;
+        for(char c: t) have[c]++;
+        int left=0;
+        int start=0;
 
-        vector<int> freq(128, 0);   // ASCII
-        for(char c : t) freq[c]++;
+        for(int right=0;right<s.length();right++){
+            required[s[right]]++;
 
-        int required = t.size();
-        int i = 0, j = 0;
-        int minLen = INT_MAX;
-        int start = 0;
-
-        while(j < s.size()){
-            // expand window
-            if(freq[s[j]] > 0) required--;
-            freq[s[j]]--;
-            j++;
-
-            // shrink window
-            while(required == 0){
-                if(j - i < minLen){
-                    minLen = j - i;
-                    start = i;
+            while(sahi(have,required)){
+                int len=right-left+1;
+                if(len<minlen){
+                    minlen=len;
+                    start=left;
                 }
-
-                freq[s[i]]++;
-                if(freq[s[i]] > 0) required++;
-                i++;
+                required[s[left]]--;
+                left++;
             }
+
         }
 
-        return (minLen == INT_MAX) ? "" : s.substr(start, minLen);
+
+
+       return minlen==INT_MAX?"":s.substr(start,minlen);
+
     }
 };
